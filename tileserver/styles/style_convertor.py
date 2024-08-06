8981b41c90743b23cfe6db9f16520ab67c3c2c3a
marziyeh <marziyehbashiri@gmail.com>
2024-08-07 00:09:17 +0330

    remove tile

--- tileserver/styles/style_convertor.py
+++ tileserver/styles/style_convertor.py
@@ -1,11 +1,18 @@
 import json
 
 file_name = 'token_day_1.json'
+server_source_file = 'server_source.json'
 token_file = 'token.json'
+converted_style_file = file_name.replace('token_', '')
+converted_to_server_style_file = file_name.replace('token_', 'server_')
 with open(file_name, 'r') as f:
     data = json.load(f)
+with open(server_source_file, 'r') as f:
+    server_source = json.load(f)
+
 with open(token_file, 'r') as f:
     token = json.load(f)
+
 key_token = tuple(token.keys())
 for layer in data['layers']:
     if layer.get('paint'):
@@ -23,6 +30,14 @@ for layer in data['layers']:
             elif type(v) != list and v in key_token:
                 paint[k] = token[v]
 
-converted_style_file = file_name.replace('token_', '')
+
+
+
 with open(converted_style_file, 'w') as f:
+    f.write(json.dumps(data))
+
+
+data["source"] = server_source
+
+with open(converted_to_server_style_file, 'w') as f:
     f.write(json.dumps(data))
